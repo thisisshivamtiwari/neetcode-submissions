0a44b5549ed7d96a2e250a17e3089d08de27f265
Shivam Tiwari <87723994+thisisshivamtiwari@users.noreply.github.com>
2026-08-31 09:06:42 +0530

    Add: search-for-word-ii - submission-0

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-0.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        ROWS, COLS = len(board), len(board[0])
+        res = []
+
+        def backtrack(r, c, i):
+            if i == len(word):
+                return True
+            if (r < 0 or c < 0 or r >= ROWS or
+                c >= COLS or board[r][c] != word[i]
+            ):
+                return False
+
+            board[r][c] = '*'
+            ret = (backtrack(r + 1, c, i + 1) or
+                   backtrack(r - 1, c, i + 1) or
+                   backtrack(r, c + 1, i + 1) or
+                   backtrack(r, c - 1, i + 1))
+            board[r][c] = word[i]
+            return ret
+
+        for word in words:
+            flag = False
+            for r in range(ROWS):
+                if flag:
+                    break
+                for c in range(COLS):
+                    if board[r][c] != word[0]:
+                        continue
+                    if backtrack(r, c, 0):
+                        res.append(word)
+                        flag = True
+                        break
+        return res
